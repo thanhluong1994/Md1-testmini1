0ccaa21cd906be2c6b73c3c6c962cc430a149da7
unknown <luongvu2605@gmail.com>
2022-01-13 18:49:15 +0700

    baitestmini

--- mini1.js
+++ mini1.js
@@ -1,5 +1,7 @@
 
-let numbers = [1,2,3,4,5,6,7,8,9,10];
-for (let i = 1; i <=10 ; i++) {
-        if (i%2==0) document.writeln(i);
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+for (let i = 1; i<=numbers.length ; i++) {
+        if (i%2==0){
+                document.writeln(i);
+}
 }
